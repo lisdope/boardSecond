0966753ecd108855bfc0c4c94c080db1e0ae1577
OliviamkChoi <oliviamkchoi@gmail.com>
2021-04-19 17:13:27 +0900

    20210419WEBV7

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,7 +1,25 @@
 package com.springbook.view.board;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAOMybatis;
 
+@Controller
 public class BoardController {
+	
+	//글 목록검색 getBoardList.do 로 지정 부탁드립니다.-cmk 
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAOMybatis boardDao) {
+		boardDao.updateBoard(vo);
+		return "getBoardList.do";
+	}
+
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAOMybatis boardDao) {
+		boardDao.deleteBoard(vo);
+		return "getBoardList.do";
+	}
 	
 }
